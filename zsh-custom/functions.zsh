28fc8d25bc6ca5e096cb927ea8f9845cd9b7bc60
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-11-16 23:49:23 +0100

    Change update order

--- zsh-custom/functions.zsh
+++ zsh-custom/functions.zsh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 
 update() {
-	update_brew
 	update_omz
+	update_brew
 	update_rust
+}
+
+update_omz() {
+	omz update
 }
 
 update_brew() {
 	brew update
 	brew upgrade
 	brew cleanup
-}
-
-update_omz() {
-	omz update
 }
 
 update_rust() {
