ea7b41a49e73b0de62c3f4c50e7c0131dbbda89b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-02-03 19:01:07 +0100

    Remove pylint

--- zsh-custom/functions.zsh
+++ zsh-custom/functions.zsh
@@ -35,9 +35,3 @@ sshk() {
 generate_django_secret_key() {
 	export DJANGO_SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
 }
-
-pylint() {
-	black .
-	isort .
-	flake8
-}
