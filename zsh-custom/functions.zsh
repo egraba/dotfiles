149f6d40851a5a8dfff646bdbe3503df8c72b85f
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-07 20:02:25 +0200

    Remove export

--- zsh-custom/functions.zsh
+++ zsh-custom/functions.zsh
@@ -33,5 +33,5 @@ sshk() {
 # Python-specific
 #
 generate_django_secret_key() {
-	export DJANGO_SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+	python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 }
