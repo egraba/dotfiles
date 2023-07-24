477511e920ffd8c92e70538eb35228121f9cb0cd
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-24 18:32:42 +0200

    Add nvm installation

--- install.sh
+++ install.sh
@@ -58,5 +58,19 @@ if [ "$1" = "poetry" ]; then
 	fi
 fi
 
+# NVM
+if [ "$1" = "nvm" ]; then
+	if ! (( $+commands[nvm] )); then
+		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+		export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+		nvm install 16
+		nvm install default 16
+
+		npm install --global yarn
+	fi
+fi
+
 # Apply changes.
 exec $SHELL
