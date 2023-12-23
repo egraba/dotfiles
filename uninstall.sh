da31d21eaa6bcbfda40abd19566ba38ccf546c46
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-12-23 09:10:22 +0100

    feat(uninstall): .zprofile is cleaned after homebrew uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,8 @@
 # Homebrew
 if (( $+commands[brew] )); then
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+	sed -i '' -e '/homebrew/d' ~/.zprofile
+	sed -i '' -e '/^$/d' ~/.zprofile
 fi
 
 # omz
