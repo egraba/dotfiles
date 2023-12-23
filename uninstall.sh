55ee6efe1898afdf1dede953f93192c05a348e13
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-12-23 09:13:30 +0100

    style(uninstall): add comment

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 # Homebrew
 if (( $+commands[brew] )); then
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+	# Clean .zprofile to avoid errors display at terminal startup.
 	sed -i '' -e '/homebrew/d' ~/.zprofile
 	sed -i '' -e '/^$/d' ~/.zprofile
 fi
