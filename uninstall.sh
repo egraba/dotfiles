223b497b883e2c909b0522d475502f4a348cb563
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-12-23 09:19:10 +0100

    feat(uninstall): cleanup .zshrc files

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,12 @@
 # Homebrew
 if (( $+commands[brew] )); then
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-	# Clean .zprofile to avoid errors display at terminal startup.
+	# Cleanup .zprofile to avoid errors display at terminal startup.
 	sed -i '' -e '/homebrew/d' ~/.zprofile
 	sed -i '' -e '/^$/d' ~/.zprofile
 fi
 
 # omz
 sh $ZSH/tools/uninstall.sh
+# Cleanup all .zshrc files (including backups from omz uninstall).
+find . -type f \( -name ".zshrc*" \) -delete
