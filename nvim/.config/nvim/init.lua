899378cef70a2d183ba2d74d5c5444f9fd2e80a9
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2026-02-14 14:15:10 +0100

    add LazyVim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,2 @@
--- ============================================================
---  Neovim configuration — Lua + lazy.nvim
--- ============================================================
-
--- Leader must be set before plugins
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Load core config
-require("config.lazy")    -- plugin manager bootstrap + setup
-require("config.options") -- editor options
-require("config.keymaps") -- key mappings
-require("config.autocmds") -- autocommands
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,55 +1,8 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
--- Highlight on yank
-autocmd("TextYankPost", {
-  group = augroup("YankHighlight", { clear = true }),
-  callback = function()
-    vim.hl.on_yank({ timeout = 200 })
-  end,
-})
-
--- Remove trailing whitespace on save
-autocmd("BufWritePre", {
-  group = augroup("TrimWhitespace", { clear = true }),
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
-})
-
--- Return to last edit position when opening files
-autocmd("BufReadPost", {
-  group = augroup("RestoreCursor", { clear = true }),
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- Auto-resize splits when terminal is resized
-autocmd("VimResized", {
-  group = augroup("AutoResize", { clear = true }),
-  command = "tabdo wincmd =",
-})
-
--- Set filetype-specific options
-autocmd("FileType", {
-  group = augroup("FileTypeSettings", { clear = true }),
-  pattern = { "python" },
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.colorcolumn = "88"
-  end,
-})
-
-autocmd("FileType", {
-  group = augroup("WebFileTypes", { clear = true }),
-  pattern = { "html", "css", "javascript", "typescript", "json", "yaml", "lua" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end,
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
